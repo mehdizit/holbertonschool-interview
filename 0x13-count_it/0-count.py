5efc203afb7b578fd884af4898f0b21795a8e3f0
mehdizit <zitounimehdi.it@gmail.com>
2021-12-09 22:57:23 +0100

    up

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,49 +1,59 @@
 #!/usr/bin/python3
 """
-a recursive function that queries the Reddit API,
-parses the title of all hot articles,
-and prints a sorted count of given keywords
+0x13. Count it! - Write a recursive function that queries the Reddit API,
+                  parses the title of all hot articles, and prints a sorted
+                  count of given keywords (case-insensitive, delimited by
+                  spaces.
+                  Javascript should count as javascript, but java should not).
 """
+
 import requests
+import sys
 
 
-def count_words(subreddit, word_list):
-    """
-    a recursive function that queries the Reddit API,
-    parses the title of all hot articles,
-    and prints a sorted count of given keywords
-    """
-    r = requests.get('https://www.reddit.com/r/{}/hot/.json'.format(subreddit),
-                     headers={'User-agent': 'Chrome'})
-    data = {}
-    titles = []
-    counts = {}
-    for word in word_list:
-        if word not in counts:
-            counts[word] = 0
-    if r.status_code == 200:
-        children = r.json().get('data').get('children')
-        for item in children:
-            titles.append(item.get('data').get('title'))
-        for title in titles:
-            for k, v in counts.items():
-                copy = title[:]
-                cut = copy.lower().split(k.lower())
-                counts[k] += len(cut) - 1
-        duplicates = {}
-        for k in counts:
-            if counts[k] == 0:
-                pass
-            elif k.lower() in duplicates:
-                duplicates[k.lower()] += counts[k]
-            else:
-                duplicates[k.lower()] = counts[k]
-        sorted_values = sorted(duplicates.values(), reverse=True)
-        sorted_dict = {}
+def count_words(subreddit, word_list, kw_cont={}, next_pg=None, reap_kw={}):
+    """all hot posts by keyword"""
+    headers = {"User-Agent": "nildiert"}
 
-        for i in sorted_values:
-            for k in duplicates.keys():
-                if duplicates[k] == i:
-                    sorted_dict[k] = duplicates[k]
-        for i in sorted_dict.keys():
-            print("{}: {}".format(i, sorted_dict[i]))
+    if next_pg:
+        subr = requests.get('https://reddit.com/r/' + subreddit +
+                            '/hot.json?after=' + next_pg, headers=headers)
+    else:
+        subr = requests.get('https://reddit.com/r/' + subreddit +
+                            '/hot.json', headers=headers)
+
+    if subr.status_code == 404:
+        return
+
+    if kw_cont == {}:
+        for word in word_list:
+            kw_cont[word] = 0
+            reap_kw[word] = word_list.count(word)
+
+    subr_dict = subr.json()
+    subr_data = subr_dict['data']
+    next_pg = subr_data['after']
+    subr_posts = subr_data['children']
+
+    for post in subr_posts:
+        post_data = post['data']
+        post_title = post_data['title']
+        title_words = post_title.split()
+        for w in title_words:
+            for key in kw_cont:
+                if w.lower() == key.lower():
+                    kw_cont[key] += 1
+
+    if next_pg:
+        count_words(subreddit, word_list, kw_cont, next_pg, reap_kw)
+
+    else:
+        for key, val in reap_kw.items():
+            if val > 1:
+                kw_cont[key] *= val
+
+        sorted_abc = sorted(kw_cont.items(), key=lambda x: x[0])
+        sorted_res = sorted(sorted_abc, key=lambda x: (-x[1], x[0]))
+        for res in sorted_res:
+            if res[1] > 0:
+                print('{}: {}'.format(res[0], res[1]))
